2c4dbf67d3a96eb919de2f98596489c21510a76a
YeHwanLee <139995082+YeHwanLee@users.noreply.github.com>
2024-07-25 22:59:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 student_data = {
-    1: {"name": "슈퍼맨", "score": {"국어": 90, "수학": 65}},
-    2: {"name": "배트맨", "score": {"국어": 75, "영어": 80, "수학": 75}}
+    1: {"name": "이예환", "score": {"국어": 90, "수학": 100}},
+    2: {"name": "이혜원", "score": {"국어": 75, "영어": 100, "수학": 75}}
 }
 @app.route('/')
 def index():
