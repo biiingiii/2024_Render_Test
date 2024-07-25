4482a260f5ed755c18d57ca72b78740a910915ce
biiingiii <lhw806@naver.com>
2024-07-25 23:25:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 student_data = {
-    1: {"name": "이예환", "score": {"국어": 90, "수학": 100}},
+    1: {"name": "이예환", "score": {"국어": 90, "영어": 00, "수학": 100}},
     2: {"name": "이혜원", "score": {"국어": 75, "영어": 100, "수학": 75}}
 }
 @app.route('/')
